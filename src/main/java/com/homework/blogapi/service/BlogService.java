294b2c97a58065125bd6ba9ff7ad55e3c338a75a
Angel Borja <borjaangel244@gmail.com>
2023-06-07 11:58:33 -0400

    slight changes

--- src/main/java/com/homework/blogapi/service/BlogService.java
+++ src/main/java/com/homework/blogapi/service/BlogService.java
@@ -1,5 +1,6 @@
 package com.homework.blogapi.service;
 
+import com.homework.blogapi.exceptions.ResourceNotFoundException;
 import com.homework.blogapi.models.Blog;
 import com.homework.blogapi.repos.BlogRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,20 +22,30 @@ public class BlogService {
         return blogRepo.findAll();
     }
 
-    public Optional<Blog> findBlogById(Long id){
-        return blogRepo.findById(id);
+    public Optional<Blog> findBlogById(Long blogId){
+        verifyBlog(blogId);
+        return blogRepo.findById(blogId);
     }
 
-    public void updateBlog(Long id, Blog blog){
-        blog.setId(id);
+    public void updateBlog(Long blogId, Blog blog){
+        verifyBlog(blogId);
+        blog.setId(blogId);
         blogRepo.save(blog);
     }
 
-    public void deleteBlog(Long id){
-        blogRepo.deleteById(id);
+    public void deleteBlog(Long blogId){
+        verifyBlog(blogId);
+        blogRepo.deleteById(blogId);
     }
 
     public Blog findBlogsByAuthor(String author){
         return blogRepo.findByAuthor(author);
+    }
+
+    protected void verifyBlog(Long blogId) throws ResourceNotFoundException {
+        Optional<Blog> blog = blogRepo.findById(blogId);
+        if (blog.isEmpty()){
+            throw new ResourceNotFoundException("The blog with id " + blogId + " does not exist :(");
+        }
     }
 }
